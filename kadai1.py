2b94a8907948fc2cb615d2420f2c4c2f797a19dd
mikirupann <sw.miki.0907@icloud.com>
2022-02-03 23:19:59 +0900

    終わりました

--- kadai1.py
+++ kadai1.py
@@ -1,8 +1,14 @@
+import time
+
 import requests
 
-url = "https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty"
+url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
 response = requests.get(url)
 results = response.json()
-list = []
+
 for i in results:
-    print(i['kids'])
+    url2 = f"https://hacker-news.firebaseio.com/v0/item/{i}.json?print=pretty"
+    reponse2 = requests.get(url2)
+    results2 = reponse2.json()
+    print(f"'title': {results2['title']}, 'link': {results2['url']}")
+    time.sleep(1)  # ここで1秒止まる
