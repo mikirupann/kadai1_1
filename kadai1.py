4864a86f93695b184e6785f01db7e6a8e6a22f08
mikirupann <sw.miki.0907@icloud.com>
2022-02-04 11:48:31 +0900

    修正完了

--- kadai1.py
+++ kadai1.py
@@ -2,13 +2,25 @@ import time
 
 import requests
 
-url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
-response = requests.get(url)
-results = response.json()
 
-for i in results:
-    url2 = f"https://hacker-news.firebaseio.com/v0/item/{i}.json?print=pretty"
-    reponse2 = requests.get(url2)
-    results2 = reponse2.json()
-    print(f"'title': {results2['title']}, 'link': {results2['url']}")
-    time.sleep(1)  # ここで1秒止まる
+def news_add():
+    url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
+    response = requests.get(url)
+    results = response.json()[0:30]
+    for i in results:
+        url2 = f"https://hacker-news.firebaseio.com/v0/item/{i}.json?print=pretty"
+        reponse2 = requests.get(url2)
+        results2 = reponse2.json()
+        try:
+            print(f"'title': {results2['title']}, 'link': {results2['url']}")
+            time.sleep(1)  # ここで1秒止まる
+        except KeyError:
+            pass
+
+
+def main():
+    news_add()
+
+
+if __name__ == '__main__':
+    main()
